17a68f0e77d2189545cf149db66bfa2e91184d99
Kayque-Silva <kayque563@gmail.com>
2023-07-25 21:19:16 -0300

    Exercicio03
    Soma de dois números.

---
+++ exercicio03.js
@@ -0,0 +1,5 @@
+const prompt= require("prompt-sync")()
+var numero1 = Number(prompt("Digite um número"));
+var numero2 = Number(prompt("Digite outro número"));
+var resultado = numero1 + numero2 
+console.log(`O resultado é, ${resultado}`);
--- index.js
+++
@@ -1,3 +0,0 @@
-const prompt = require('prompt-sync')();
-var nome = prompt("Qual é seu nome?");
-console.log(`Olá, ${nome}! bom te conhcer`);
